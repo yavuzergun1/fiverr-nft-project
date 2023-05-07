3bbb01eb5ce5aab780318fa51b936ec1a96bb513
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-05-07 21:00:12 +0300

    routes done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./routes/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import About from "./routes/About";
+import Faq from "./routes/Faq";
+import Contact from "./routes/Contact";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<About />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/contactus" element={<Contact />} />
         </Routes>
         <Footer/>
       </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,39 @@ import Button from "./Button";
 const Navbar = () => {
 
   return (
-    <nav className="w-full h-28 bg-[#232345]">
+    <nav className="w-full h-28 bg-[#232345] flex justify-between items-center px-7">
+      <h2 className="text-white">LOGO</h2>
+      <div className="links w-4/12 flex justify-between">
+
       <NavLink
         exact
         to="/"
-        className={({ isActive }) => (isActive ? "active font-roboto" : "")}
-      >
+        className={({ isActive }) => (isActive ? "active " : "text-white")}
+        >
         Home
       </NavLink>
+      <NavLink
+        exact
+        to="/aboutus"
+        className={({ isActive }) => (isActive ? "active " : "text-white")}
+        >
+        About Us
+      </NavLink>
+      <NavLink
+        exact
+        to="/faq"
+        className={({ isActive }) => (isActive ? "active " : "text-white")}
+        >
+        FAQ
+      </NavLink>
+      <NavLink
+        exact
+        to="/contactus"
+        className={({ isActive }) => (isActive ? "active " : "text-white")}
+        >
+        Contact Us
+      </NavLink>
+        </div>
       <Button addClass={"bg-[#0055FF] w-[148px] h-[40px]"}> Login</Button>
     </nav>
   );
