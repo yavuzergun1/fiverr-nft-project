a69ee5e1e6ae254037f4e3855a54ee17faeb781f
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-05-08 21:43:12 +0300

    navbar hamburger menu done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,126 @@
 import { NavLink } from "react-router-dom";
 import Button from "./Button";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
+  const [isOpen, setIsOpen] = useState(false);
+  function handleMenuClick() {
+    setIsOpen(!isOpen);
+  }
   return (
-    <nav className="w-full h-28 bg-[#232345] flex justify-between items-center px-7">
+    <nav className="w-full h-28 bg-[#232345] flex justify-between items-center px-3 sm:px-7">
       <h2 className="text-white">LOGO</h2>
-      <div className="links w-4/12 flex justify-between">
+      <div className="links w-5/12 hidden sm:flex justify-between">
+        <NavLink
+          exact
+          to="/"
+          className={({ isActive }) => (isActive ? "active " : "text-white")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          exact
+          to="/aboutus"
+          className={({ isActive }) => (isActive ? "active " : "text-white")}
+        >
+          About Us
+        </NavLink>
+        <NavLink
+          exact
+          to="/faq"
+          className={({ isActive }) => (isActive ? "active " : "text-white")}
+        >
+          FAQ
+        </NavLink>
+        <NavLink
+          exact
+          to="/contactus"
+          className={({ isActive }) => (isActive ? "active " : "text-white")}
+        >
+          Contact Us
+        </NavLink>
+      </div>
+      <Button
+        addClass={
+          "bg-[#0055FF] w-[148px] h-[40px] absolute right-16 sm:relative "
+        }
+      >
+        {" "}
+        Login
+      </Button>
 
-      <NavLink
-        exact
-        to="/"
-        className={({ isActive }) => (isActive ? "active " : "text-white")}
+      <div className="hamburger-menu relative sm:hidden inline-block ">
+        <input
+          type="checkbox"
+          checked={isOpen}
+          onChange={handleMenuClick}
+          className="hidden"
+          id="menuToggle"
+        />
+        <label
+          htmlFor="menuToggle"
+          className={`block text-sm cursor-pointer ${
+            isOpen && "relative z-20 "
+          }`}
         >
-        Home
-      </NavLink>
-      <NavLink
-        exact
-        to="/aboutus"
-        className={({ isActive }) => (isActive ? "active " : "text-white")}
+          <span
+            className={`block w-5 h-[3px] mb-[3px] ${
+              isOpen
+                ? "bg-gray-500 relative z-20 rotate-45 translate-y-[5px] transition-all"
+                : "bg-white z-50 transition-all"
+            }`}
+          ></span>
+          <span
+            className={`block  w-5 h-[3px] mb-[3px] ${
+              isOpen ? "transition-all hidden" : "bg-white z-50 transition-all"
+            }`}
+          ></span>
+          <span
+            className={`block  w-5 h-[3px] mb-1 ${
+              isOpen
+                ? "bg-gray-500 relative z-20 -rotate-45 -translate-y-[1px] transition-all "
+                : "bg-white z-50 transition-all"
+            }`}
+          ></span>
+        </label>
+        <div
+          className={`absolute flex flex-col -top-14 left-0 h-screen z-10 bg-white shadow-lg p-4 transition-all duration-300 ease-in-out ${
+            isOpen ? "-translate-x-52 w-screen" : "translate-x-52"
+          }`}
         >
-        About Us
-      </NavLink>
-      <NavLink
-        exact
-        to="/faq"
-        className={({ isActive }) => (isActive ? "active " : "text-white")}
-        >
-        FAQ
-      </NavLink>
-      <NavLink
-        exact
-        to="/contactus"
-        className={({ isActive }) => (isActive ? "active " : "text-white")}
-        >
-        Contact Us
-      </NavLink>
+          <Link
+            to="/"
+            onClick={handleMenuClick}
+            className="my-2 text-gray-800 mt-16 font-medium hover:text-primary"
+          >
+            Home
+          </Link>
+          <Link
+            onClick={handleMenuClick}
+            to="/aboutus"
+            className="my-2 text-gray-800 font-medium hover:text-primary"
+          >
+            About Us
+          </Link>
+          <Link
+            onClick={handleMenuClick}
+            to="/contactus"
+            className="my-2 text-gray-800 font-medium hover:text-primary"
+          >
+            Contact Us
+          </Link>
+          <Link
+            onClick={handleMenuClick}
+            to="/faq"
+            className="my-2 text-gray-800 font-medium hover:text-primary"
+          >
+            FAQs{" "}
+          </Link>
         </div>
-      <Button addClass={"bg-[#0055FF] w-[148px] h-[40px]"}> Login</Button>
+      </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,33 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="bg-[#181931] w-full h-full text-white pb-24">
+      <div className="top-container flex">
+        <div className="left-container px-2 sm:px-28 ">
+          <h1 className="text-6xl w-[590px] leading-snug font-bold pt-[233px]">
+            Opinions Traded Wealth Create GetFoz
+          </h1>
+          <p className="text-3xl font-light w-[590px] mt-10">
+            Experience seamless trading on our secure platform and trade with
+            confidence.
+          </p>
+          <button className="bg-[#0055FF] w-[253px] h-[53px] rounded-2xl mt-10">
+            Connect Wallet
+          </button>
+          <div className="trademarks">
+            <div className="first">
+              <img src="../assets/path16.png" alt="" />
+              <img src="../assets/Vector.png" alt="" />
+              <img src="../assets/Vector (1).png" alt="" />
+              <img src="../assets/Vector (2).png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="right-container"></div>
+      </div>
+      <div className="h-52"></div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
